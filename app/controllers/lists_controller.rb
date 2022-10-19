151522c76dbc00116c9b45823190f314f6e48afc
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-10-19 20:36:03 +0900

    first commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,14 @@
 class ListsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  # private
+
+  # def prototype_params
+  #   params.require(:list).permit(:nickname).merge(user_id: current_user.id)
+  # end
+
 end
