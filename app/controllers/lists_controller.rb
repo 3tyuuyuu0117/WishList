3d66ac79a7acf39baf5c9d1eef6e867305f0566e
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-10-22 19:21:09 +0900

    投稿画面

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,25 @@
 class ListsController < ApplicationController
 
   def index
+    @lists = List.includes(:user)
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:nickname, :image).merge(user_id: current_user.id)
+    params.require(:list).permit(:title, :detail, :image).merge(user_id: current_user.id)
   end
-
 end
